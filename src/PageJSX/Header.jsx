f27ff57f852c474a6581a6820656fadcc094f71e
Andreas <andmarknu@hotmail.com>
2023-11-14 14:21:37 +0100

    14.11.20.23 14:21 CD

--- src/PageJSX/Header.jsx
+++ src/PageJSX/Header.jsx
@@ -3,7 +3,7 @@ import './../StylesSCSS/header.scss'
 export default function Header (){
     return(
         <div className="headerWrapper">
-            <h1 >Header</h1>
+            <h1 >Header  hhhhh</h1>
         </div>
     );
 };
