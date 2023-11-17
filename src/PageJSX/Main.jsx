00d1a843aedbc00654ecd16c421d0380fbf27285
Andreas7test <andmarknutest@hotmail.com>
2023-11-17 14:24:06 +0100

    commit 17.11.2023 14:23

--- src/PageJSX/Main.jsx
+++ src/PageJSX/Main.jsx
@@ -3,8 +3,8 @@ import './../StylesSCSS/main.scss'
 export default function Main (){
     return(
         <div className="mainWrapper">
-            <h1 className="main">Main abcd 4624</h1>
-            <nav className='navbar'>Navbar 
+            <h1 className="main">Main </h1>
+            <nav className='navbar'>Navbar commit 17.11.2023 14:23
                 <ul>
                    <li>li 1 <button className='button1'></button></li>
                    <li>li 2 <button className='button2'></button></li>
